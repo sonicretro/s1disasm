9a95e4924d502a96b5a6c1eab53c284b9cc58ef9
unknown <devnull@localhost>
2011-07-26 15:07:21 +0000

    MainMemory - S2LVL: Added object definitions for Spring Yard Zone, thanks to TomoAlien.

---
+++ S1LVL INI Files/SYZ/Platform.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using SonicRetro.S2LVL;
+
+namespace S1ObjectDefinitions.SYZ
+{
+    class Platform : ObjectDefinition
+    {
+        private Point offset;
+        private BitmapBits img;
+
+        public override void Init(Dictionary<string, string> data)
+        {
+            byte[] artfile = ObjectHelper.LevelArt;
+            img = ObjectHelper.MapASMToBmp(artfile, "../_maps/Platforms (SYZ).asm", "@platform", 2, out offset);
+        }
+
+        public override ReadOnlyCollection<byte> Subtypes()
+        {
+            return new ReadOnlyCollection<byte>(new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0B, 0x0C });
+        }
+
+        public override string Name()
+        {
+            return "Platform";
+        }
+
+        public override bool RememberState()
+        {
+            return false;
+        }
+
+        public override string SubtypeName(byte subtype)
+        {
+            return ((PlatformMovement)(subtype & 0xF)).ToString();
+        }
+
+        public override string FullName(byte subtype)
+        {
+            return Name() + " - " + SubtypeName(subtype);
+        }
+
+        public override BitmapBits Image()
+        {
+            return img;
+        }
+
+        public override BitmapBits Image(byte subtype)
+        {
+            return img;
+        }
+
+        public override Rectangle Bounds(Point loc, byte subtype)
+        {
+            return new Rectangle(loc.X + offset.X, loc.Y + offset.Y, img.Width, img.Height);
+        }
+
+        public override void Draw(BitmapBits bmp, Point loc, byte subtype, bool XFlip, bool YFlip, bool includeDebug)
+        {
+            BitmapBits bits = new BitmapBits(img);
+            bits.Flip(XFlip, YFlip);
+            bmp.DrawBitmapComposited(bits, new Point(loc.X + offset.X, loc.Y + offset.Y));
+        }
+
+        public override Type ObjectType
+        {
+            get
+            {
+                return typeof(PlatformS1ObjectEntry);
+            }
+        }
+    }
+
+    public class PlatformS1ObjectEntry : S1ObjectEntry
+    {
+        public PlatformS1ObjectEntry() : base() { }
+        public PlatformS1ObjectEntry(byte[] file, int address) : base(file, address) { }
+
+        public PlatformMovement Movement
+        {
+            get
+            {
+                return (PlatformMovement)(SubType & 0xF);
+            }
+            set
+            {
+                SubType = (byte)((SubType & ~0xF) | (int)value);
+            }
+        }
+
+        public byte SwitchID
+        {
+            get
+            {
+                return (byte)(SubType >> 4);
+            }
+            set
+            {
+                SubType = (byte)((SubType & ~0xF0) | (value << 4));
+            }
+        }
+    }
+
+    public enum PlatformMovement
+    {
+        Stationary,
+        RightLeft,
+        DownUp,
+        FallStand,
+        Fall,
+        LeftRight,
+        UpDown,
+        SwitchUp,
+        MoveUp,
+        Stationary2,
+        Invalid1,
+        DownUpSlow,
+        UpDownSlow,
+        Invalid2,
+        Invalid3,
+        Invalid4
+    }
+}
