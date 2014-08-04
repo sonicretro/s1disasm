8e85c4fd94db8c731bb7d545c9515561ea8edc28
MainMemory <sonicmike2@yahoo.com>
2014-08-04 10:12:24 -0500

    Adding source code for 's1p2bin'

---
+++ AS/s1p2bin.cpp
@@ -0,0 +1,146 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h> // for unlink
+
+const char* codeFileName = NULL;
+const char* romFileName = NULL;
+
+void printUsage() { printf("usage: s2p2bin.exe inputcodefile.p outputromfile.bin\n"); }
+bool buildRom(FILE* from, FILE* to);
+
+int main(int argc, char *argv[])
+{
+//	for(int i = 0 ; i < argc ; i++)
+//		printf("arg %d is %s\n", i, argv[i]);
+
+	if(argc > 2)
+		argc--, argv++; // skip the exe filename
+
+	if(argc < 2)
+		printUsage();
+
+	while(argc)
+	{
+		char* arg = argv[0];
+		argc--, argv++; // pop arg
+		
+		if(!strcasecmp(arg, "-h") || !strcasecmp(arg, "--help"))
+			printUsage(), argc = 0;
+		else if(!codeFileName)
+			codeFileName = arg;
+		else if(!romFileName)
+			romFileName = arg;
+	}
+
+	if(codeFileName && romFileName)
+	{
+		printf("\ns2p2bin.exe: generating %s from %s", romFileName, codeFileName);
+		
+		FILE* from = fopen(codeFileName, "rb");
+		if(from)
+		{
+			FILE* to = fopen(romFileName, "wb");
+			if(to)
+			{
+				bool built = buildRom(from, to);
+				fclose(to);
+				fclose(from);
+				if(built)
+				{
+					printf(" ... done.");
+				}
+				else
+				{
+					unlink(romFileName); // error; delete the rom because it's probably hosed
+				}
+			}
+			else
+				printf("\nERROR: Failed to access file \"%s\".", romFileName);
+		}
+		else
+			printf("\nERROR: Failed to load file \"%s\".", codeFileName);
+	}
+	
+	printf("\n");
+//	system("PAUSE");
+	return 0;
+}
+
+bool buildRom(FILE* from, FILE* to)
+{
+	if(fgetc(from) != 0x89) printf("\nWarning: First byte of a .p file should be $89");
+	if(fgetc(from) != 0x14) printf("\nWarning: Second byte of a .p file should be $14");
+	
+	int cpuType = 0, segmentType = 0, granularity = 0;
+	signed long start = 0;
+	unsigned short length = 0;
+	static const int scratchSize = 4096;
+	unsigned char scratch [scratchSize];
+	
+	while(true)
+	{
+		unsigned char headerByte = fgetc(from);
+		if(ferror(from) || feof(from))
+			break;
+
+		switch(headerByte)
+		{
+			case 0x00: // "END" segment
+				return true;
+			case 0x80: // "entry point" segment
+				fseek(from, 3, SEEK_CUR);
+				continue;
+			case 0x81:  // code or data segment
+				cpuType = fgetc(from);
+				segmentType = fgetc(from);
+				granularity = fgetc(from);
+				if(granularity != 1)
+					{ printf("\nERROR: Unsupported granularity %d.", granularity); return false; }
+				break;
+			default:
+				if(headerByte > 0x81)
+					{ printf("\nERROR: Unsupported segment header $%02X", headerByte); return false; }
+				cpuType = headerByte;
+				break;
+		}
+
+		start = fgetc(from); // integers in AS .p files are always little endian
+		start |= fgetc(from) << 8;
+		start |= fgetc(from) << 16;
+		start |= fgetc(from) << 24;
+		length = fgetc(from);
+		length |= fgetc(from) << 8;
+
+		if(length == 0)
+		{
+			// error instead of warning because I had quite a bad freeze the one time I saw this warning go off
+			printf("\nERROR: zero length segment ($%X).", length);
+			return false;
+		}
+
+		if(start < 0)
+		{
+			printf("\nERROR: negative start address ($%X).", start), start = 0;
+			return false;
+		}
+
+		if(start+3 < ftell(to)) // 3 bytes of leeway for instruction patching
+			printf("\nWarning: overlapping allocation detected! $%X < $%X", start, ftell(to));
+
+		fseek(to, start, SEEK_SET);
+
+//		printf("copying $%X-$%X -> $%X-$%X\n", ftell(from), ftell(from)+length, start, start+length);
+		while(length)
+		{
+			int copy = length;
+			if(copy > scratchSize)
+				copy = scratchSize;
+			fread(scratch, copy, 1, from);
+			fwrite(scratch, copy, 1, to);
+			length -= copy;
+		}
+		
+	}
+
+	return true;
+}
