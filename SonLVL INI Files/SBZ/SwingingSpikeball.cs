6acebbc18478ba29824466482accda4bc33f94b4
Clownacy <fred.arnold101@googlemail.com>
2015-05-12 22:46:24 +0100

    SonLVL - Giving SBZ an obj15 definition

---
+++ SonLVL INI Files/SBZ/SwingingSpikeball.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using SonicRetro.SonLVL.API;
+using System;
+
+namespace S1ObjectDefinitions.SBZ
+{
+    class SwingingSpikeball : ObjectDefinition
+    {
+        private int[] labels = { 0, 1, 2 };
+        private Sprite img;
+        private List<Sprite> imgs = new List<Sprite>();
+
+        public override void Init(ObjectData data)
+        {
+            byte[] artfile = ObjectHelper.OpenArtFile("../artnem/SYZ Large Spikeball.bin", CompressionType.Nemesis);
+            img = ObjectHelper.MapASMToBmp(artfile, "../_maps/Big Spiked Ball.asm", 0, 0);
+            for (int i = 0; i < labels.Length; i++)
+                imgs.Add(ObjectHelper.MapASMToBmp(artfile, "../_maps/Big Spiked Ball.asm", labels[i], i == 2 ? 2 : 0));
+        }
+
+        public override ReadOnlyCollection<byte> Subtypes
+        {
+            get { return new ReadOnlyCollection<byte>(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }); }
+        }
+
+        public override string Name
+        {
+            get { return "Swinging Spikeball"; }
+        }
+
+        public override bool RememberState
+        {
+            get { return false; }
+        }
+
+        public override string SubtypeName(byte subtype)
+        {
+            return (subtype & 15) + " links";
+        }
+
+        public override Sprite Image
+        {
+            get { return img; }
+        }
+
+        public override Sprite SubtypeImage(byte subtype)
+        {
+            return img;
+        }
+
+        public override Rectangle GetBounds(ObjectEntry obj, Point camera)
+        {
+            return new Rectangle(obj.X + img.Offset.X - camera.X, obj.Y + img.Offset.Y - camera.Y, img.Image.Width, img.Image.Height + (imgs[1].Image.Height * (obj.SubType & 15)) + imgs[2].Image.Height - 24);
+        }
+
+        public override Sprite GetSprite(ObjectEntry obj)
+        {
+            int length = obj.SubType & 15;
+            List<Sprite> sprs = new List<Sprite>();
+            sprs.Add(imgs[2]);
+            int yoff = 16;
+            for (int i = 0; i < length; i++)
+            {
+                sprs.Add(new Sprite(imgs[1].Image, new Point(imgs[1].X, yoff + imgs[1].Y)));
+                yoff += 16;
+            }
+            yoff -= 8;
+            sprs.Add(new Sprite(imgs[0].Image, new Point(imgs[0].X, yoff + imgs[0].Y)));
+            Sprite spr = new Sprite(sprs.ToArray());
+            spr.Offset = new Point(spr.X + obj.X, spr.Y + obj.Y);
+            return spr;
+        }
+    }
+}
