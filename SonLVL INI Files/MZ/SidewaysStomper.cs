c9d6980677f44d861d9c75ed90346d539690b6d3
Clownacy <fred.arnold101@googlemail.com>
2015-05-24 01:49:36 +0100

    SonLVL - Correcting tabs/spaces in SidewaysStomper.cs

--- SonLVL INI Files/MZ/SidewaysStomper.cs
+++ SonLVL INI Files/MZ/SidewaysStomper.cs
@@ -65,7 +65,7 @@ namespace S1ObjectDefinitions.MZ
                 Sprite spr = new Sprite(sprs.ToArray());
                 spr.Offset = new Point(spr.X, spr.Y);
                 return spr;
-	    }
+            }
         }
 
         public override Sprite SubtypeImage(byte subtype)
@@ -98,17 +98,17 @@ namespace S1ObjectDefinitions.MZ
             switch (obj.SubType)
             {
                 default:
-		    secondaryxoff = -0x38;
-		    sprs.Add(new Sprite(imgs[3].Image, new Point(globalxoff + secondaryxoff + 0x34 + imgs[3].X, imgs[3].Y)));
-		    break;
+                    secondaryxoff = -0x38;
+                    sprs.Add(new Sprite(imgs[3].Image, new Point(globalxoff + secondaryxoff + 0x34 + imgs[3].X, imgs[3].Y)));
+                    break;
                 case 01:
-		    secondaryxoff = -0xA0;
-		    sprs.Add(new Sprite(imgs[5].Image, new Point(globalxoff + secondaryxoff + 0x34 + imgs[5].X, imgs[5].Y)));
-		    break;
+                    secondaryxoff = -0xA0;
+                    sprs.Add(new Sprite(imgs[5].Image, new Point(globalxoff + secondaryxoff + 0x34 + imgs[5].X, imgs[5].Y)));
+                    break;
                 case 02:
-		    secondaryxoff = -0x50;
-		    sprs.Add(new Sprite(imgs[4].Image, new Point(globalxoff + secondaryxoff + 0x34 + imgs[4].X, imgs[4].Y)));
-		    break;
+                    secondaryxoff = -0x50;
+                    sprs.Add(new Sprite(imgs[4].Image, new Point(globalxoff + secondaryxoff + 0x34 + imgs[4].X, imgs[4].Y)));
+                    break;
             }
             sprs.Add(new Sprite(imgs[2].Image, new Point(globalxoff + 0x28 + imgs[2].X, imgs[2].Y)));
             sprs.Add(new Sprite(imgs[1].Image, new Point(globalxoff + secondaryxoff - 0x1C + imgs[1].X, imgs[1].Y)));
