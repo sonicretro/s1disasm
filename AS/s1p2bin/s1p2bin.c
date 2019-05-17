a37f494107d6d67f90cec80717584c6fde9d7ee8
Clownacy <Clownacy@users.noreply.github.com>
2019-05-17 22:49:45 +0000

    Removed extraneous semicolon in s1p2bin.c

--- AS/s1p2bin/s1p2bin.c
+++ AS/s1p2bin/s1p2bin.c
@@ -81,7 +81,7 @@ int main(int argc, char *argv[])
 
 void CompressZ80Driver(FILE *to)
 {
-	unsigned char *compressed_buffer;;
+	unsigned char *compressed_buffer;
 
 	if (accurate_compression)
 		compressedLength = AccurateKosinskiCompress(Z80_RAM_buffer, current_Z80_size, &compressed_buffer);
