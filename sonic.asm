8a3e8f0fb590d142c61f4711327f8c8f0cc8e110
StardustGear <stargear12@gmail.com>
2019-12-11 21:22:44 +0200

    Documented BuildSprites

--- sonic.asm
+++ sonic.asm
@@ -6278,100 +6278,103 @@ BuildSprites:
 		lea	(v_spritequeue).w,a4
 		moveq	#7,d7
 
-loc_D66A:
-		tst.w	(a4)
-		beq.w	loc_D72E
+	@priorityLoop:
+		tst.w	(a4)	; are there objects left to draw?
+		beq.w	@nextPriority	; if not, branch
 		moveq	#2,d6
 
-loc_D672:
-		movea.w	(a4,d6.w),a0
-		tst.b	(a0)
-		beq.w	loc_D726
-		bclr	#7,obRender(a0)
+	@objectLoop:
+		movea.w	(a4,d6.w),a0	; load object ID
+		tst.b	(a0)		; if null, branch
+		beq.w	@skipObject
+		bclr	#7,obRender(a0)		; set as not visible
+
 		move.b	obRender(a0),d0
 		move.b	d0,d4
-		andi.w	#$C,d0
-		beq.s	loc_D6DE
+		andi.w	#$C,d0		; get drawing coordinates
+		beq.s	@screenCoords	; branch if 0 (screen coordinates)
 		movea.l	BldSpr_ScrPos(pc,d0.w),a1
+	; check object bounds
 		moveq	#0,d0
 		move.b	obActWid(a0),d0
 		move.w	obX(a0),d3
 		sub.w	(a1),d3
 		move.w	d3,d1
 		add.w	d0,d1
-		bmi.w	loc_D726
+		bmi.w	@skipObject	; left edge out of bounds
 		move.w	d3,d1
 		sub.w	d0,d1
-		cmpi.w	#$140,d1
-		bge.s	loc_D726
-		addi.w	#$80,d3
-		btst	#4,d4
-		beq.s	loc_D6E8
+		cmpi.w	#320,d1
+		bge.s	@skipObject	; right edge out of bounds
+		addi.w	#128,d3		; VDP sprites start at 128px
+
+		btst	#4,d4		; is assume height flag on?
+		beq.s	@assumeHeight	; if yes, branch
 		moveq	#0,d0
 		move.b	obHeight(a0),d0
 		move.w	obY(a0),d2
 		sub.w	4(a1),d2
 		move.w	d2,d1
 		add.w	d0,d1
-		bmi.s	loc_D726
+		bmi.s	@skipObject	; top edge out of bounds
 		move.w	d2,d1
 		sub.w	d0,d1
-		cmpi.w	#$E0,d1
-		bge.s	loc_D726
-		addi.w	#$80,d2
-		bra.s	loc_D700
+		cmpi.w	#224,d1
+		bge.s	@skipObject
+		addi.w	#128,d2		; VDP sprites start at 128px
+		bra.s	@drawObject
 ; ===========================================================================
 
-loc_D6DE:
-		move.w	$A(a0),d2
+	@screenCoords:
+		move.w	$A(a0),d2	; special variable for screen Y
 		move.w	obX(a0),d3
-		bra.s	loc_D700
+		bra.s	@drawObject
 ; ===========================================================================
 
-loc_D6E8:
+	@assumeHeight:
 		move.w	obY(a0),d2
 		sub.w	obMap(a1),d2
 		addi.w	#$80,d2
 		cmpi.w	#$60,d2
-		blo.s	loc_D726
+		blo.s	@skipObject
 		cmpi.w	#$180,d2
-		bhs.s	loc_D726
+		bhs.s	@skipObject
 
-loc_D700:
+	@drawObject:
 		movea.l	obMap(a0),a1
 		moveq	#0,d1
-		btst	#5,d4
-		bne.s	loc_D71C
+		btst	#5,d4		; is static mappings flag on?
+		bne.s	@drawFrame	; if yes, branch
 		move.b	obFrame(a0),d1
 		add.b	d1,d1
-		adda.w	(a1,d1.w),a1
-		move.b	(a1)+,d1
+		adda.w	(a1,d1.w),a1	; get mappings frame address
+		move.b	(a1)+,d1	; number of sprite pieces
 		subq.b	#1,d1
-		bmi.s	loc_D720
+		bmi.s	@setVisible
 
-loc_D71C:
-		bsr.w	sub_D750
+	@drawFrame:
+		bsr.w	BuildSpr_Draw	; write data from sprite pieces to buffer
 
-loc_D720:
-		bset	#7,obRender(a0)
+	@setVisible:
+		bset	#7,obRender(a0)		; set object as visible
 
-loc_D726:
+	@skipObject:
 		addq.w	#2,d6
-		subq.w	#2,(a4)
-		bne.w	loc_D672
+		subq.w	#2,(a4)			; number of objects left
+		bne.w	@objectLoop
 
-loc_D72E:
+	@nextPriority:
 		lea	$80(a4),a4
-		dbf	d7,loc_D66A
+		dbf	d7,@priorityLoop
 		move.b	d5,(v_spritecount).w
 		cmpi.b	#$50,d5
-		beq.s	loc_D748
+		beq.s	@spriteLimit
 		move.l	#0,(a2)
 		rts	
 ; ===========================================================================
 
-loc_D748:
-		move.b	#0,-5(a2)
+	@spriteLimit:
+		move.b	#0,-5(a2)	; set last sprite link
 		rts	
 ; End of function BuildSprites
 
@@ -6379,95 +6382,132 @@ loc_D748:
 ; ||||||||||||||| S U B	R O U T	I N E |||||||||||||||||||||||||||||||||||||||
 
 
-sub_D750:
+BuildSpr_Draw:
 		movea.w	obGfx(a0),a3
 		btst	#0,d4
-		bne.s	loc_D796
+		bne.s	BuildSpr_FlipX
 		btst	#1,d4
-		bne.w	loc_D7E4
-; End of function sub_D750
+		bne.w	BuildSpr_FlipY
+; End of function BuildSpr_Draw
 
 
 ; ||||||||||||||| S U B	R O U T	I N E |||||||||||||||||||||||||||||||||||||||
 
 
-sub_D762:
-		cmpi.b	#$50,d5
-		beq.s	locret_D794
-		move.b	(a1)+,d0
+BuildSpr_Normal:
+		cmpi.b	#$50,d5		; check sprite limit
+		beq.s	@return
+		move.b	(a1)+,d0	; get y-offset
 		ext.w	d0
-		add.w	d2,d0
-		move.w	d0,(a2)+
-		move.b	(a1)+,(a2)+
-		addq.b	#1,d5
-		move.b	d5,(a2)+
-		move.b	(a1)+,d0
+		add.w	d2,d0		; add y-position
+		move.w	d0,(a2)+	; write to buffer
+		move.b	(a1)+,(a2)+	; write sprite size
+		addq.b	#1,d5		; increase sprite counter
+		move.b	d5,(a2)+	; set as sprite link
+		move.b	(a1)+,d0	; get art tile
 		lsl.w	#8,d0
 		move.b	(a1)+,d0
-		add.w	a3,d0
-		move.w	d0,(a2)+
-		move.b	(a1)+,d0
+		add.w	a3,d0		; add art tile offset
+		move.w	d0,(a2)+	; write to buffer
+		move.b	(a1)+,d0	; get x-offset
 		ext.w	d0
-		add.w	d3,d0
-		andi.w	#$1FF,d0
-		bne.s	loc_D78E
+		add.w	d3,d0		; add x-position
+		andi.w	#$1FF,d0	; keep within 512px
+		bne.s	@writeX
 		addq.w	#1,d0
 
-loc_D78E:
-		move.w	d0,(a2)+
-		dbf	d1,sub_D762
+	@writeX:
+		move.w	d0,(a2)+	; write to buffer
+		dbf	d1,BuildSpr_Normal	; process next sprite piece
 
-locret_D794:
+	@return:
 		rts	
-; End of function sub_D762
+; End of function BuildSpr_Normal
 
 ; ===========================================================================
 
-loc_D796:
-		btst	#1,d4
-		bne.w	loc_D82A
+BuildSpr_FlipX:
+		btst	#1,d4		; is object also y-flipped?
+		bne.w	BuildSpr_FlipXY	; if yes, branch
 
-loc_D79E:
-		cmpi.b	#$50,d5
-		beq.s	locret_D7E2
-		move.b	(a1)+,d0
+	@loop:
+		cmpi.b	#$50,d5		; check sprite limit
+		beq.s	@return
+		move.b	(a1)+,d0	; y position
 		ext.w	d0
 		add.w	d2,d0
 		move.w	d0,(a2)+
-		move.b	(a1)+,d4
-		move.b	d4,(a2)+
-		addq.b	#1,d5
+		move.b	(a1)+,d4	; size
+		move.b	d4,(a2)+	
+		addq.b	#1,d5		; link
 		move.b	d5,(a2)+
-		move.b	(a1)+,d0
+		move.b	(a1)+,d0	; art tile
 		lsl.w	#8,d0
-		move.b	(a1)+,d0
+		move.b	(a1)+,d0	
 		add.w	a3,d0
-		eori.w	#$800,d0
-		move.w	d0,(a2)+
-		move.b	(a1)+,d0
+		eori.w	#$800,d0	; toggle flip-x in VDP
+		move.w	d0,(a2)+	; write to buffer
+		move.b	(a1)+,d0	; get x-offset
 		ext.w	d0
-		neg.w	d0
-		add.b	d4,d4
+		neg.w	d0			; negate it
+		add.b	d4,d4		; calculate flipped position by size
 		andi.w	#$18,d4
 		addq.w	#8,d4
 		sub.w	d4,d0
 		add.w	d3,d0
-		andi.w	#$1FF,d0
-		bne.s	loc_D7DC
+		andi.w	#$1FF,d0	; keep within 512px
+		bne.s	@writeX
 		addq.w	#1,d0
 
-loc_D7DC:
-		move.w	d0,(a2)+
-		dbf	d1,loc_D79E
+	@writeX:
+		move.w	d0,(a2)+	; write to buffer
+		dbf	d1,@loop		; process next sprite piece
 
-locret_D7E2:
+	@return:
 		rts	
 ; ===========================================================================
 
-loc_D7E4:
-		cmpi.b	#$50,d5
-		beq.s	locret_D828
+BuildSpr_FlipY:
+		cmpi.b	#$50,d5		; check sprite limit
+		beq.s	@return
+		move.b	(a1)+,d0	; get y-offset
+		move.b	(a1),d4		; get size
+		ext.w	d0
+		neg.w	d0		; negate y-offset
+		lsl.b	#3,d4	; calculate flip offset
+		andi.w	#$18,d4
+		addq.w	#8,d4
+		sub.w	d4,d0
+		add.w	d2,d0	; add y-position
+		move.w	d0,(a2)+	; write to buffer
+		move.b	(a1)+,(a2)+	; size
+		addq.b	#1,d5
+		move.b	d5,(a2)+	; link
+		move.b	(a1)+,d0	; art tile
+		lsl.w	#8,d0
 		move.b	(a1)+,d0
+		add.w	a3,d0
+		eori.w	#$1000,d0	; toggle flip-y in VDP
+		move.w	d0,(a2)+
+		move.b	(a1)+,d0	; x-position
+		ext.w	d0
+		add.w	d3,d0
+		andi.w	#$1FF,d0
+		bne.s	@writeX
+		addq.w	#1,d0
+
+	@writeX:
+		move.w	d0,(a2)+	; write to buffer
+		dbf	d1,BuildSpr_FlipY	; process next sprite piece
+
+	@return:
+		rts	
+; ===========================================================================
+
+BuildSpr_FlipXY:
+		cmpi.b	#$50,d5		; check sprite limit
+		beq.s	@return
+		move.b	(a1)+,d0	; calculated flipped y
 		move.b	(a1),d4
 		ext.w	d0
 		neg.w	d0
@@ -6476,55 +6516,18 @@ loc_D7E4:
 		addq.w	#8,d4
 		sub.w	d4,d0
 		add.w	d2,d0
-		move.w	d0,(a2)+
-		move.b	(a1)+,(a2)+
+		move.w	d0,(a2)+	; write to buffer
+		move.b	(a1)+,d4	; size
+		move.b	d4,(a2)+	; link
 		addq.b	#1,d5
-		move.b	d5,(a2)+
+		move.b	d5,(a2)+	; art tile
 		move.b	(a1)+,d0
 		lsl.w	#8,d0
 		move.b	(a1)+,d0
 		add.w	a3,d0
-		eori.w	#$1000,d0
+		eori.w	#$1800,d0	; toggle flip-x/y in VDP
 		move.w	d0,(a2)+
-		move.b	(a1)+,d0
-		ext.w	d0
-		add.w	d3,d0
-		andi.w	#$1FF,d0
-		bne.s	loc_D822
-		addq.w	#1,d0
-
-loc_D822:
-		move.w	d0,(a2)+
-		dbf	d1,loc_D7E4
-
-locret_D828:
-		rts	
-; ===========================================================================
-
-loc_D82A:
-		cmpi.b	#$50,d5
-		beq.s	locret_D87C
-		move.b	(a1)+,d0
-		move.b	(a1),d4
-		ext.w	d0
-		neg.w	d0
-		lsl.b	#3,d4
-		andi.w	#$18,d4
-		addq.w	#8,d4
-		sub.w	d4,d0
-		add.w	d2,d0
-		move.w	d0,(a2)+
-		move.b	(a1)+,d4
-		move.b	d4,(a2)+
-		addq.b	#1,d5
-		move.b	d5,(a2)+
-		move.b	(a1)+,d0
-		lsl.w	#8,d0
-		move.b	(a1)+,d0
-		add.w	a3,d0
-		eori.w	#$1800,d0
-		move.w	d0,(a2)+
-		move.b	(a1)+,d0
+		move.b	(a1)+,d0	; calculate flipped x
 		ext.w	d0
 		neg.w	d0
 		add.b	d4,d4
@@ -6533,14 +6536,14 @@ loc_D82A:
 		sub.w	d4,d0
 		add.w	d3,d0
 		andi.w	#$1FF,d0
-		bne.s	loc_D876
+		bne.s	@writeX
 		addq.w	#1,d0
 
-loc_D876:
-		move.w	d0,(a2)+
-		dbf	d1,loc_D82A
+	@writeX:
+		move.w	d0,(a2)+	; write to buffer
+		dbf	d1,BuildSpr_FlipXY	; process next sprite piece
 
-locret_D87C:
+	@return:
 		rts	
 
 		include	"_incObj\sub ChkObjectVisible.asm"
@@ -7928,7 +7931,7 @@ loc_1B210:
 		move.b	(a1)+,d1
 		subq.b	#1,d1
 		bmi.s	loc_1B268
-		jsr	(sub_D762).l
+		jsr	(BuildSpr_Normal).l
 
 loc_1B268:
 		addq.w	#4,a4
