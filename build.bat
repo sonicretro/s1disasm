1bc5e088784a4fb7889293e9b9b2017864151310
Tails8521 <tails8521@gmail.com>
2021-09-09 17:12:23 +0200

    Fixed label case inconsistencies

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 
 IF EXIST s1built.bin move /Y s1built.bin s1built.prev.bin >NUL
-asm68k /k /p /o ae- sonic.asm, s1built.bin >errors.txt, , sonic.lst
+asm68k /k /p /o ae- /o c+ sonic.asm, s1built.bin >errors.txt, , sonic.lst
 fixheadr.exe s1built.bin
