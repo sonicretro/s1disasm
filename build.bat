sonicretro/s1disasm

commit 65cb7e56c35b2aa57746d1759dedec5859e0d392

Merged build and build_plus
Now, like s2disasm, the DAC driver compressor is selected by passing
the '-a' argument to build.bat or build.py.

build.bat
@@ -25,14 +25,24 @@ REM // -L generates a listing file
 set AS_MSGPATH=AS/Win32
 set USEANSI=n
 
+set s1p2bin_args=
+
+:parseloop
+IF "%1"=="-a" (
+	set s1p2bin_args=-a
+	echo Will use accurate sound driver compression
+)
+SHIFT
+IF NOT "%1"=="" goto parseloop
+
 REM // allow the user to choose to output error messages to file by supplying the -logerrors parameter
-IF "%1"=="-logerrors" ( "AS/Win32/asw.exe" -xx -q -A -L sonic.asm ) ELSE "AS/Win32/asw.exe" -xx -q -E -A -L sonic.asm
+"AS/Win32/asw.exe" -xx -q -E -A -L sonic.asm
 
 REM // if there were errors, a log file is produced
 IF EXIST sonic.log goto LABLERROR3
 
 REM // combine the assembler output into a ROM
-IF EXIST sonic.p "AS/Win32/s1p2bin" -a sonic.p s1built.bin
+IF EXIST sonic.p "AS/Win32/s1p2bin" %s1p2bin_args% sonic.p s1built.bin
 
 REM // done -- pause if we seem to have failed, then exit
 IF NOT EXIST sonic.p goto LABLPAUSE

build.py
@@ -1,8 +1,150 @@
 #!/usr/bin/python3
 
+import argparse
+import os
+import platform
 import sys
 sys.dont_write_bytecode = True
+from subprocess import call
+import subprocess
+import hashlib
 
-import build_defs
+# Paths to build tools, depending on OS
 
-build_defs.run(False)
+if platform.system() == "Windows":
+	asBinary = "AS/Win32/asw.exe";
+	s1p2binBinary = "AS/Win32/s1p2bin.exe";
+
+elif platform.system() == "Darwin": # Osx
+	asBinary = "AS/Osx/asl";
+	s1p2binBinary = "AS/Osx/s1p2bin";
+
+elif platform.system() == "Linux":
+	if sys.maxsize > 2**32:	# detect if running in 64-bit environment
+		asBinary = "AS/Linux/asl";
+		s1p2binBinary = "AS/Linux/s1p2bin";
+	else:
+		asBinary = "AS/Linux32/asl";
+		s1p2binBinary = "AS/Linux32/s1p2bin";
+
+else:
+	print("Unknown platform");
+	sys.exit();
+
+def delete(path):
+	if os.path.isfile(path):
+		os.remove(path);
+
+def move(path1, path2):
+	if os.path.isfile(path1):
+		os.rename(path1, path2);
+
+def update_checksum(path):
+	with open(path, "r+b") as file:
+		# Read the whole file in memory
+		bytes = file.read();
+
+		# Calculate the checksum
+		checksum = 0;
+		for i in range(0x200, len(bytes), 2):
+			checksum += (bytes[i + 0] << 8) + (bytes[i + 1] << 0);
+
+		# Write the checksum to the header
+		file.seek(0x18E);
+		file.write(bytearray([(checksum >> 8) & 0xFF, (checksum >> 0) & 0xFF]));
+
+def check_bitperfect(path):
+	hashobj = hashlib.sha256();
+
+	with open(path, "rb") as file:
+		hashobj.update(file.read());
+
+	if hashobj.hexdigest() == "46160baa06362c711c9f1a5017cb7371026444936c8af5e93a78996cf32ff2a6":
+		print("  New build is bit-perfect with REV00");
+	elif hashobj.hexdigest() == "1b7f6635bd713f37f3c2f44f302b872c2e3c5f56e63637918dad4637146900fd":
+		print("  New build is bit-perfect with REV01");
+	elif hashobj.hexdigest() == "94a5379fa0bd6760cb0606b77f705a85b6af1724da99e8c765b34ba2ab825e1a":
+		print("  New build is bit-perfect with REVXB");
+	else:
+		print("  New build is not bit-perfect with REV00, REV01, or REVXB");
+
+
+# Define and parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--accurate", action="store_true", help="compress the DAC driver in a way that's accurate to the original ROM (uses sub-optimal compression)")
+args = parser.parse_args()
+
+if platform.system() == "Windows":
+	os.environ["AS_MSGPATH"] = "AS/Win32";
+	os.environ["USEANSI"] = "n";
+
+# Build ROM
+print("Building s1built");
+
+# Create full paths for all files
+romPath = "s1built.bin";	
+romPathPrev = "s1built.prev.bin";
+errorsPath = "sonic.log";
+outputPath = "_Output.txt";
+binaryOutputPath = "_BinaryOutput.txt";
+
+print("  Cleaning previous build");
+# Remove old output
+delete(romPathPrev);
+move(romPath, romPathPrev);
+delete(errorsPath);
+delete(outputPath);
+delete(binaryOutputPath);
+delete("sonic.p");
+
+assembleCommand = [asBinary, "-xx", "-n", "-q", "-A", "-L", "-U"];
+
+# Input asm file
+assembleCommand.append("sonic.asm");
+
+print("  Assembling .p file");
+
+assembleProcess = subprocess.Popen(assembleCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
+output, errors = assembleProcess.communicate();
+with open(errorsPath, "wb") as errorsFile:
+	errorsFile.write(errors);
+with open(outputPath, "wb") as outputFile:
+	outputFile.write(output);
+
+# Detect if there was an error
+if os.path.isfile("sonic.p") == False:
+	print("There was a fatal error during assembly: see sonic.log for details");
+	sys.exit();
+
+# Create binary
+
+if args.accurate:
+	binaryCommand = [s1p2binBinary, "-a", "sonic.p", romPath];
+else:
+	binaryCommand = [s1p2binBinary, "sonic.p", romPath];
+
+# Output file
+
+print("  Converting .p to .bin");
+
+binaryProcess = subprocess.Popen(binaryCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, errors = binaryProcess.communicate()
+outputFile = open(binaryOutputPath, "wb")
+outputFile.write(output)
+outputFile.close()
+
+# Fixing checksum
+
+print("  Calculating checksum");
+update_checksum(romPath);
+
+print("  Removing temporary files");
+
+# delete working files
+delete("sonic.p");
+
+# Ensure bit-perfection
+if args.accurate:
+	check_bitperfect(romPath);
+
+print("Finished!");

build_defs.py
@@ -1,147 +1,0 @@
-#!/usr/bin/python3
-
-import os
-import platform
-import sys
-from subprocess import call
-import subprocess
-import hashlib
-
-# Paths to build tools, depending on OS
-
-if platform.system() == "Windows":
-	asBinary = "AS/Win32/asw.exe";
-	s1p2binBinary = "AS/Win32/s1p2bin.exe";
-	s1p2binplusBinary = "AS/Win32/s1p2bin_plus.exe";
-
-elif platform.system() == "Darwin": # Osx
-	asBinary = "AS/Osx/asl";
-	s1p2binBinary = "AS/Osx/s1p2bin";
-	s1p2binplusBinary = "AS/Osx/s1p2bin_plus";
-
-elif platform.system() == "Linux":
-	if sys.maxsize > 2**32:	# detect if running in 64-bit environment
-		asBinary = "AS/Linux/asl";
-		s1p2binBinary = "AS/Linux/s1p2bin";
-		s1p2binplusBinary = "AS/Linux/s1p2bin_plus";
-	else:
-		asBinary = "AS/Linux32/asl";
-		s1p2binBinary = "AS/Linux32/s1p2bin";
-		s1p2binplusBinary = "AS/Linux32/s1p2bin_plus";
-
-else:
-	print("Unknown platform");
-	sys.exit();
-
-def delete(path):
-	if os.path.isfile(path):
-		os.remove(path);
-
-def move(path1, path2):
-	if os.path.isfile(path1):
-		os.rename(path1, path2);
-
-def update_checksum(path):
-	with open(path, "r+b") as file:
-		# Read the whole file in memory
-		bytes = file.read();
-
-		# Calculate the checksum
-		checksum = 0;
-		for i in range(0x200, len(bytes), 2):
-			checksum += (bytes[i + 0] << 8) + (bytes[i + 1] << 0);
-
-		# Write the checksum to the header
-		file.seek(0x18E);
-		file.write(bytearray([(checksum >> 8) & 0xFF, (checksum >> 0) & 0xFF]));
-
-def check_bitperfect(path):
-	hashobj = hashlib.sha256();
-
-	with open(path, "rb") as file:
-		hashobj.update(file.read());
-
-	if hashobj.hexdigest() == "46160baa06362c711c9f1a5017cb7371026444936c8af5e93a78996cf32ff2a6":
-		print("  New build is bit-perfect with REV00");
-	elif hashobj.hexdigest() == "1b7f6635bd713f37f3c2f44f302b872c2e3c5f56e63637918dad4637146900fd":
-		print("  New build is bit-perfect with REV01");
-	elif hashobj.hexdigest() == "94a5379fa0bd6760cb0606b77f705a85b6af1724da99e8c765b34ba2ab825e1a":
-		print("  New build is bit-perfect with REVXB");
-	else:
-		print("  New build is not bit-perfect with REV00, REV01, or REVXB");
-
-def run(altcomp):
-	if platform.system() == "Windows":
-		os.environ["AS_MSGPATH"] = "AS/Win32";
-		os.environ["USEANSI"] = "n";
-
-	# Build ROM
-	print("Building s1built");
-
-	# Create full paths for all files
-	romPath = "s1built.bin";	
-	romPathPrev = "s1built.prev.bin";
-	errorsPath = "sonic.log";
-	outputPath = "_Output.txt";
-	binaryOutputPath = "_BinaryOutput.txt";
-
-	print("  Cleaning previous build");
-	# Remove old output
-	delete(romPathPrev);
-	move(romPath, romPathPrev);
-	delete(errorsPath);
-	delete(outputPath);
-	delete(binaryOutputPath);
-	delete("sonic.p");
-
-	assembleCommand = [asBinary, "-xx", "-n", "-q", "-A", "-L", "-U"];
-
-	# Input asm file
-	assembleCommand.append("sonic.asm");
-
-	print("  Assembling .p file");
-
-	assembleProcess = subprocess.Popen(assembleCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
-	output, errors = assembleProcess.communicate();
-	with open(errorsPath, "wb") as errorsFile:
-		errorsFile.write(errors);
-	with open(outputPath, "wb") as outputFile:
-		outputFile.write(output);
-
-	# Detect if there was an error
-	if os.path.isfile("sonic.p") == False:
-		print("There was a fatal error during assembly: see sonic.log for details");
-		sys.exit();
-
-	# Create binary
-
-	if altcomp:
-		binaryCommand = [s1p2binplusBinary, "sonic.p", romPath];
-	else:
-		binaryCommand = [s1p2binBinary, "sonic.p", romPath];
-
-	# Output file
-
-	print("  Converting .p to .bin");
-
-	binaryProcess = subprocess.Popen(binaryCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	output, errors = binaryProcess.communicate()
-	outputFile = open(binaryOutputPath, "wb")
-	outputFile.write(output)
-	outputFile.close()
-
-	# Fixing checksum
-
-	print("  Calculating checksum");
-	update_checksum(romPath);
-
-	print("  Removing temporary files");
-
-	# delete working files
-	delete("sonic.p");
-
-	# Ensure bit-perfection
-	if not altcomp:
-		check_bitperfect(romPath);
-
-	print("Finished!");

build_plus.bat
@@ -1,72 +1,0 @@
-@ECHO OFF
-
-REM // make sure we can write to the file s1built.bin
-REM // also make a backup to s1built.prev.bin
-IF NOT EXIST s1built.bin goto LABLNOCOPY
-IF EXIST s1built.prev.bin del s1built.prev.bin
-IF EXIST s1built.prev.bin goto LABLNOCOPY
-move /Y s1built.bin s1built.prev.bin
-IF EXIST s1built.bin goto LABLERROR2
-:LABLNOCOPY
-
-REM // delete some intermediate assembler output just in case
-IF EXIST sonic.p del sonic.p
-IF EXIST sonic.p goto LABLERROR1
-
-REM // clear the output window
-cls
-
-REM // run the assembler
-REM // -xx shows the most detailed error output
-REM // -q makes AS shut up
-REM // -E outputs error messages to file
-REM // -A gives us a small speedup
-REM // -L generates a listing file
-set AS_MSGPATH=AS/Win32
-set USEANSI=n
-
-REM // allow the user to choose to output error messages to file by supplying the -logerrors parameter
-IF "%1"=="-logerrors" ( "AS/Win32/asw.exe" -xx -q -A -L sonic.asm ) ELSE "AS/Win32/asw.exe" -xx -q -E -A -L sonic.asm
-
-REM // if there were errors, a log file is produced
-IF EXIST sonic.log goto LABLERROR3
-
-REM // combine the assembler output into a ROM
-IF EXIST sonic.p "AS/Win32/s1p2bin" sonic.p s1built.bin
-
-REM // done -- pause if we seem to have failed, then exit
-IF NOT EXIST sonic.p goto LABLPAUSE
-IF NOT EXIST s1built.bin goto LABLPAUSE
-fixheader s1built.bin
-exit /b
-:LABLPAUSE
-
-pause
-
-
-exit /b
-
-:LABLERROR1
-echo Failed to build because write access to sonic.p was denied.
-pause
-
-
-exit /b
-
-:LABLERROR2
-echo Failed to build because write access to s1built.bin was denied.
-pause
-
-exit /b
-
-:LABLERROR3
-REM // display a noticeable message
-echo.
-echo *************************************************************************
-echo *                                                                       *
-echo *   There were build errors/warnings. See sonic.log for more details.   *
-echo *                                                                       *
-echo *************************************************************************
-echo.
-pause
-

build_plus.py
@@ -1,8 +1,0 @@
-#!/usr/bin/python3
-
-import sys
-sys.dont_write_bytecode = True
-
-import build_defs
-
-build_defs.run(True)

chkbitperfect.rev00.bat
@@ -1,7 +1,7 @@
 @echo OFF
 
 REM // build the ROM
-call build
+call build -a
 
 REM  // run fc against a Sonic 1 Rev 00 ROM
 echo -------------------------------------------------------------

chkbitperfect.rev01.bat
@@ -1,7 +1,7 @@
 @echo OFF
 
 REM // build the ROM
-call build
+call build -a
 
 REM  // run fc against a Sonic 1 Rev 01 ROM
 echo -------------------------------------------------------------

commit 03ceb06de0047d252e5ca5bfe11c22a93661b2b0

Fix build.bat when errors occur
It seems that build.bat has been broken since
65cb7e56c35b2aa57746d1759dedec5859e0d392, all the way back in 2019...

Oops.

Anyway, this seems to fix it. I don't know exactly *why* this was
breaking it, but it appears to work now.

build.bat
@@ -41,11 +41,13 @@ REM // allow the user to choose to output error messages to file by supplying th
 REM // if there were errors, a log file is produced
 IF EXIST sonic.log goto LABLERROR3
 
+REM // if there were errors, there won't be any sonic.p output
+IF NOT EXIST sonic.p goto LABLPAUSE
+
 REM // combine the assembler output into a ROM
-IF EXIST sonic.p "AS/Win32/s1p2bin" %s1p2bin_args% sonic.p s1built.bin
+"AS/Win32/s1p2bin" %s1p2bin_args% sonic.p s1built.bin
 
 REM // done -- pause if we seem to have failed, then exit
-IF NOT EXIST sonic.p goto LABLPAUSE
 IF NOT EXIST s1built.bin goto LABLPAUSE
 fixheader s1built.bin
 exit /b