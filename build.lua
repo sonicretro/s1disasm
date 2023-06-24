96eaa40284214a9e7d06b4535ec2d36771524333
Clownacy <Clownacy@users.noreply.github.com>
2023-06-24 16:59:11 +0100

    Fix ROM header not being patched.
    The script was calling `os.exit` before patching the ROM header.

--- build.lua
+++ build.lua
@@ -15,7 +15,17 @@ local improved_dac_driver_compression = false
 
 local common = require "build_tools.lua.common"
 
-os.exit(common.build_rom("sonic", "s1built", "", "-p=FF -z=0," .. (improved_dac_driver_compression and "kosinski-optimised" or "kosinski") .. ",Size_of_DAC_driver_guess,after", false, "https://github.com/sonicretro/s1disasm"))
+local success, continue = common.build_rom("sonic", "s1built", "", "-p=FF -z=0," .. (improved_dac_driver_compression and "kosinski-optimised" or "kosinski") .. ",Size_of_DAC_driver_guess,after", false, "https://github.com/sonicretro/s1disasm")
+
+if not success then
+	exit_code = false
+end
+
+if not continue then
+	os.exit(false)
+end
 
 -- Correct the ROM's header with a proper checksum and end-of-ROM value.
 common.fix_header("s1built.bin")
+
+os.exit(exit_code)
