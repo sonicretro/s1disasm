79325ec99c17e9a3cd85e8a87d49c19c3eb14239
Clownacy <Clownacy@users.noreply.github.com>
2023-08-25 20:12:01 +0100

    Deobfuscate FindFreeObj.

--- _incObj/sub FindFreeObj.asm
+++ _incObj/sub FindFreeObj.asm
@@ -10,10 +10,10 @@
 
 FindFreeObj:
 		lea	(v_lvlobjspace).w,a1 ; start address for object RAM
-		move.w	#$5F,d0
+		move.w	#(v_lvlobjend-v_lvlobjspace)/object_size-1,d0
 
 FFree_Loop:
-		tst.b	(a1)		; is object RAM	slot empty?
+		tst.b	obID(a1)		; is object RAM	slot empty?
 		beq.s	FFree_Found	; if yes, branch
 		lea	object_size(a1),a1	; goto next object RAM slot
 		dbf	d0,FFree_Loop	; repeat $5F times
@@ -43,7 +43,7 @@ FindNextFreeObj:
 		bcs.s	NFree_Found
 
 NFree_Loop:
-		tst.b	(a1)
+		tst.b	obID(a1)
 		beq.s	NFree_Found
 		lea	object_size(a1),a1
 		dbf	d0,NFree_Loop
