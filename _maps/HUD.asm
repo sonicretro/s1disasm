34ca42ec40e4aae40580242297575eebb445820f
Mildanner <cianocriativo408@gmail.com>
2026-04-28 19:20:34 -0300

    Document every part of mapping in HUD

--- _maps/HUD.asm
+++ _maps/HUD.asm
@@ -8,55 +8,55 @@ Map_HUD_internal:	mappingsTable
 	mappingsTableEntry.w	.allred
 
 .allyellow:	spriteHeader
-	spritePiece	0, -$80, 4, 2, 0, 0, 0, 0, 1
-	spritePiece	$20, -$80, 4, 2, $18, 0, 0, 0, 1
-	spritePiece	$40, -$80, 4, 2, $20, 0, 0, 0, 1
-	spritePiece	0, -$70, 4, 2, $10, 0, 0, 0, 1
-	spritePiece	$28, -$70, 4, 2, $28, 0, 0, 0, 1
-	spritePiece	0, -$60, 4, 2, 8, 0, 0, 0, 1
-	spritePiece	$20, -$60, 1, 2, 0, 0, 0, 0, 1
-	spritePiece	$30, -$60, 3, 2, $30, 0, 0, 0, 1
-	spritePiece	0, $40, 2, 2, $10A, 0, 0, 0, 1
-	spritePiece	$10, $40, 4, 2, $10E, 0, 0, 0, 1
+	spritePiece	0, -$80, 4, 2, 0, 0, 0, 0, 1		  ; SCOR
+	spritePiece	$20, -$80, 4, 2, $18, 0, 0, 0, 1 	  ; E XXXX
+	spritePiece	$40, -$80, 4, 2, $20, 0, 0, 0, 1	  ; XXXX - Score count
+	spritePiece	0, -$70, 4, 2, $10, 0, 0, 0, 1		  ; TIME
+	spritePiece	$28, -$70, 4, 2, $28, 0, 0, 0, 1	  ; X : XX
+	spritePiece	0, -$60, 4, 2, 8, 0, 0, 0, 1		  ; RING
+	spritePiece	$20, -$60, 1, 2, 0, 0, 0, 0, 1		  ; S
+	spritePiece	$30, -$60, 3, 2, $30, 0, 0, 0, 1	  ; RING COUNT
+	spritePiece	0, $40, 2, 2, $10A, 0, 0, 0, 1		  ; HUD LIVE COUNTER MAP #1
+	spritePiece	$10, $40, 4, 2, $10E, 0, 0, 0, 1	  ; HUD LIVE COUNTER MAP #2
 .allyellow_End
 	even
 .ringred:	spriteHeader
-	spritePiece	0, -$80, 4, 2, 0, 0, 0, 0, 1
-	spritePiece	$20, -$80, 4, 2, $18, 0, 0, 0, 1
-	spritePiece	$40, -$80, 4, 2, $20, 0, 0, 0, 1
-	spritePiece	0, -$70, 4, 2, $10, 0, 0, 0, 1
-	spritePiece	$28, -$70, 4, 2, $28, 0, 0, 0, 1
-	spritePiece	0, -$60, 4, 2, 8, 0, 0, 1, 1
-	spritePiece	$20, -$60, 1, 2, 0, 0, 0, 1, 1
-	spritePiece	$30, -$60, 3, 2, $30, 0, 0, 0, 1
-	spritePiece	0, $40, 2, 2, $10A, 0, 0, 0, 1
-	spritePiece	$10, $40, 4, 2, $10E, 0, 0, 0, 1
+	spritePiece	0, -$80, 4, 2, 0, 0, 0, 0, 1		  ; SCOR
+	spritePiece	$20, -$80, 4, 2, $18, 0, 0, 0, 1 	  ; E XXXX
+	spritePiece	$40, -$80, 4, 2, $20, 0, 0, 0, 1	  ; XXXX - Score count
+	spritePiece	0, -$70, 4, 2, $10, 0, 0, 0, 1		  ; TIME
+	spritePiece	$28, -$70, 4, 2, $28, 0, 0, 0, 1	  ; X : XX
+	spritePiece	0, -$60, 4, 2, 8, 0, 0, 1, 1		  ; RING
+	spritePiece	$20, -$60, 1, 2, 0, 0, 0, 1, 1	 	  ; S
+	spritePiece	$30, -$60, 3, 2, $30, 0, 0, 0, 1	  ; RING COUNT
+	spritePiece	0, $40, 2, 2, $10A, 0, 0, 0, 1	          ; HUD LIVE COUNTER MAP #1
+	spritePiece	$10, $40, 4, 2, $10E, 0, 0, 0, 1	  ; HUD LIVE COUNTER MAP #2
 .ringred_End
 	even
 .timered:	spriteHeader
-	spritePiece	0, -$80, 4, 2, 0, 0, 0, 0, 1
-	spritePiece	$20, -$80, 4, 2, $18, 0, 0, 0, 1
-	spritePiece	$40, -$80, 4, 2, $20, 0, 0, 0, 1
-	spritePiece	0, -$70, 4, 2, $10, 0, 0, 1, 1
-	spritePiece	$28, -$70, 4, 2, $28, 0, 0, 0, 1
-	spritePiece	0, -$60, 4, 2, 8, 0, 0, 0, 1
-	spritePiece	$20, -$60, 1, 2, 0, 0, 0, 0, 1
-	spritePiece	$30, -$60, 3, 2, $30, 0, 0, 0, 1
-	spritePiece	0, $40, 2, 2, $10A, 0, 0, 0, 1
-	spritePiece	$10, $40, 4, 2, $10E, 0, 0, 0, 1
+	spritePiece	0, -$80, 4, 2, 0, 0, 0, 0, 1		  ; SCOR
+	spritePiece	$20, -$80, 4, 2, $18, 0, 0, 0, 1 	  ; E XXXX
+	spritePiece	$40, -$80, 4, 2, $20, 0, 0, 0, 1	  ; XXXX - Score count
+	spritePiece	0, -$70, 4, 2, $10, 0, 0, 1, 1		  ; TIME
+	spritePiece	$28, -$70, 4, 2, $28, 0, 0, 0, 1	  ; X : XX
+	spritePiece	0, -$60, 4, 2, 8, 0, 0, 0, 1		  ; RING
+	spritePiece	$20, -$60, 1, 2, 0, 0, 0, 0, 1		  ; S
+	spritePiece	$30, -$60, 3, 2, $30, 0, 0, 0, 1	  ; RING COUNT
+	spritePiece	0, $40, 2, 2, $10A, 0, 0, 0, 1		  ; HUD LIVE COUNTER MAP #1
+	spritePiece	$10, $40, 4, 2, $10E, 0, 0, 0, 1	  ; HUD LIVE COUNTER MAP #2
 .timered_End
 	even
 .allred:	spriteHeader
-	spritePiece	0, -$80, 4, 2, 0, 0, 0, 0, 1
-	spritePiece	$20, -$80, 4, 2, $18, 0, 0, 0, 1
-	spritePiece	$40, -$80, 4, 2, $20, 0, 0, 0, 1
-	spritePiece	0, -$70, 4, 2, $10, 0, 0, 1, 1
-	spritePiece	$28, -$70, 4, 2, $28, 0, 0, 0, 1
-	spritePiece	0, -$60, 4, 2, 8, 0, 0, 1, 1
-	spritePiece	$20, -$60, 1, 2, 0, 0, 0, 1, 1
-	spritePiece	$30, -$60, 3, 2, $30, 0, 0, 0, 1
-	spritePiece	0, $40, 2, 2, $10A, 0, 0, 0, 1
-	spritePiece	$10, $40, 4, 2, $10E, 0, 0, 0, 1
+	spritePiece	0, -$80, 4, 2, 0, 0, 0, 0, 1		  ; SCOR
+	spritePiece	$20, -$80, 4, 2, $18, 0, 0, 0, 1	  ; E XXXX
+	spritePiece	$40, -$80, 4, 2, $20, 0, 0, 0, 1          ; XXXX - Score count
+	spritePiece	0, -$70, 4, 2, $10, 0, 0, 1, 1		  ; TIME
+	spritePiece	$28, -$70, 4, 2, $28, 0, 0, 0, 1	  ; X : XX
+	spritePiece	0, -$60, 4, 2, 8, 0, 0, 1, 1		  ; RING
+	spritePiece	$20, -$60, 1, 2, 0, 0, 0, 1, 1		  ; S
+	spritePiece	$30, -$60, 3, 2, $30, 0, 0, 0, 1	  ; RING COUNT
+	spritePiece	0, $40, 2, 2, $10A, 0, 0, 0, 1            ; HUD LIVE COUNTER MAP #1
+	spritePiece	$10, $40, 4, 2, $10E, 0, 0, 0, 1          ; HUD LIVE COUNTER MAP #2
 .allred_End
 
 	even
